94ebb864acdb57c6eea35bf05710e0776f95c743
roque <roquevaamonde@gmail.com>
2021-12-05 02:37:08 +0100

    Modificado sh

--- secure_https_ip.sh
+++ secure_https_ip.sh
@@ -6,15 +6,15 @@ historico=$confd/ip_historica.ip
 domain="dominio.ddns.net"
 dns="8.8.8.8"
 ip_actual=$(nslookup $domain | grep Address |  grep -v $dns | awk '{ print $NF }')
+plantilla="ex_default"
 
 if [ $ip_actual != $(cat $historico) ];
   then
     echo $ip_actual > $historico
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $confd/privateKey.key -out $confd/certificate.crt -subj "/CN=$ip_actual"
     export IP_PUBLICA=$ip_actual
-    envsubst '${IP_PUBLICA}' < $confd/ex_default > $confd/default.conf
-    cd $workdir
-    docker-compose restart
+    envsubst '${IP_PUBLICA}' < $confd/$plantilla > $confd/default.conf
+    $workdir/docker-compose restart
     exit 0
 else
     exit 0
